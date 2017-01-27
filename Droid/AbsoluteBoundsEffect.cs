eb862325279745894c698217d81a1d64adc629f6
Daiki Kawanuma <daiki.kawanuma@gmail.com>
2017-01-27 17:49:50 +0900

    reformat

--- Droid/AbsoluteBoundsEffect.cs
+++ Droid/AbsoluteBoundsEffect.cs
@@ -5,45 +5,42 @@ using Xamarin.Forms.Platform.Android;
 
 [assembly: ResolutionGroupName("Santea")]
 [assembly: ExportEffect(typeof(ViewAbsoluteBoundsEffect), "ViewAbsoluteBoundsEffect")]
+
 namespace ActivityTransitionSample.Droid
 {
-	public class ViewAbsoluteBoundsEffect : PlatformEffect
-	{
-		protected override void OnAttached()
-		{
-			try
-			{
-				Control.LayoutChange += (sender, e) =>
-				{
-					UpdateAbsoluteBounds();
-				};
+    public class ViewAbsoluteBoundsEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.LayoutChange += (sender, e) => { UpdateAbsoluteBounds(); };
 
-				UpdateAbsoluteBounds();
-							}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
-			}
-		}
+                UpdateAbsoluteBounds();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
 
-		protected override void OnDetached()
-		{
-			
-		}
+        protected override void OnDetached()
+        {
+        }
 
-		private void UpdateAbsoluteBounds()
-		{
-			int[] position = new int[2];
-			Control.GetLocationInWindow(position);
+        private void UpdateAbsoluteBounds()
+        {
+            var position = new int[2];
+            Control.GetLocationInWindow(position);
 
-			var activity = Forms.Context as FormsAppCompatActivity;
-			float density = activity.Resources.DisplayMetrics.Density;
+            var activity = Forms.Context as FormsAppCompatActivity;
+            var density = activity.Resources.DisplayMetrics.Density;
 
-			AbsoluteBoundsEffect.SetAbsoluteBounds(Element,
-												   new Rectangle((int)(position[0] / density + 0.5f), 
-			                                                     (int)(position[1] / density + 0.5f) - 80, 
-			                                                     (int)(Control.Width / density + 0.5f), 
-			                                                     (int)(Control.Height / density + 0.5f)));
-		}
-	}
+            AbsoluteBoundsEffect.SetAbsoluteBounds(Element,
+                new Rectangle((int) (position[0] / density + 0.5f),
+                    (int) (position[1] / density + 0.5f),
+                    (int) (Control.Width / density + 0.5f),
+                    (int) (Control.Height / density + 0.5f)));
+        }
+    }
 }
